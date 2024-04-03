161f96e002ec06fc3d4899f63759314d8944e46d
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-03 12:03:10 -0300

    removing browseTags

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -25,7 +25,6 @@ public class App {
 		//.doOnError(throwable -> logger.error("Unable to connect", throwable))
 		.ignoreElement().blockingAwait();
 
-		opcUaOperations.browseTags().foreachBlocking(System.out::println);
 	}
 }
 
