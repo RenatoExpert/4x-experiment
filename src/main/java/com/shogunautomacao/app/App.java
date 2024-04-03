c4300fe3bc6188da4449212391bd66397345af4c
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-03 11:25:13 -0300

    trying to use libs from plc4x

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -1,9 +1,15 @@
 package com.shogunautomacao.app;
 
+//import org.apache.plc4x.java.opcua.config.OpcuaConfiguration;
+
 public class App {
 	public static void main(String[] args) throws Exception {
 		String host = "ec2-3-93-58-9.compute-1.amazonaws.com";
 		System.out.println("Hello World!");
+		//OpcuaConfiguration config = new OpcuaConfiguration();
+		PlcConnection plcConnection = PlcDriverManager.getDefault().getConnectionManager().getConnection(connectionString);
+		System.out.println("Works");
+
 	}
 }
 
