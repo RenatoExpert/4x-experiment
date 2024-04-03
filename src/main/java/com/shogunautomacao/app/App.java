1419727fa2992e6f578ead58f6b68913712e71ff
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-03 11:46:40 -0300

    trying simple opc

--- src/main/java/com/shogunautomacao/app/App.java
+++ src/main/java/com/shogunautomacao/app/App.java
@@ -1,15 +1,26 @@
 package com.shogunautomacao.app;
 
-//import org.apache.plc4x.java.opcua.config.OpcuaConfiguration;
-
 public class App {
 	public static void main(String[] args) throws Exception {
-		String host = "ec2-3-93-58-9.compute-1.amazonaws.com";
+		String host = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
 		System.out.println("Hello World!");
-		//OpcuaConfiguration config = new OpcuaConfiguration();
-		PlcConnection plcConnection = PlcDriverManager.getDefault().getConnectionManager().getConnection(connectionString);
 		System.out.println("Works");
 
+		//create a connection profile
+		OpcUaConnectionProfile connectionProfile = new OpcUaConnectionProfile()
+		.withConnectionUri(URI.create(host))
+		.withClientIdUri("hurence:opc-simple:client:test")
+		.withClientName("Simple OPC test client")
+		.withSocketTimeout(Duration.ofSeconds(5));
+
+		//Create an instance of a ua operations
+		OpcUaOperations opcUaOperations = new OpcUaTemplate();
+		//connect using our profile
+		opcUaOperations.connect(connectionProfile)
+		.doOnError(throwable -> logger.error("Unable to connect", throwable))
+		.ignoreElement().blockingAwait();
+
+		opcDaOperations.browseTags().foreachBlocking(System.out::println);
 	}
 }
 
