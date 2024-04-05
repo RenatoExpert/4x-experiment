b8ed35b77927e5b364e31d5d23e6156ba41f0dd4
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-05 12:47:31 -0300

    removing some imports

--- src/main/java/org/scadalts/Main.java
+++ src/main/java/org/scadalts/Main.java
@@ -7,9 +7,6 @@ import org.apache.plc4x.java.api.exceptions.PlcConnectionException;
 import org.apache.plc4x.java.api.messages.PlcBrowseRequest;
 import org.apache.plc4x.java.api.messages.PlcBrowseResponse;
 
-import java.sql.DatabaseMetaData;
-import java.util.Iterator;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
@@ -18,7 +15,7 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 	public static void main(String[] args) throws PlcConnectionException {
-		Logger parentLogger = LogManager.getLogger();
+		Logger logger = Logger();
 		String url = "opcua://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
 		System.out.println("Hello");
 		PlcDriver driver = PlcDriverManager.getDefault().getDriver("opcua");
