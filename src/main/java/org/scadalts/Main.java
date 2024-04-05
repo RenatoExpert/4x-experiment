1b15c4e679916d31c79d38055ef3ec4b65033e73
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-05 11:50:50 -0300

    show protocol codes

--- src/main/java/org/scadalts/Main.java
+++ src/main/java/org/scadalts/Main.java
@@ -8,12 +8,18 @@ import org.apache.plc4x.java.api.messages.PlcBrowseRequest;
 import org.apache.plc4x.java.api.messages.PlcBrowseResponse;
 
 import java.sql.DatabaseMetaData;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.logging.Logger;
 
 public class Main {
 	public static void main(String[] args) throws PlcConnectionException {
 		String url = "opcua://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
-        PlcDriver driver = PlcDriverManager.getDefault().getDriver("opcua");
+		System.out.println("Hello");
+		Set<String> codes = PlcDriverManager.getDefault().getProtocolCodes();
+		Iterator<String> codesIterator = codes.iterator();
+		codesIterator.forEachRemaining(System.out::println);
         /*
         PlcConnection plcConnection = driver.getConnection(url);
 
