4da047c0addc15d7deba8f689fb85eff8af0e157
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-05 10:45:48 -0300

    removing imports, to let intellij work on it later

--- src/main/java/org/scadalts/Main.java
+++ src/main/java/org/scadalts/Main.java
@@ -2,12 +2,6 @@ package org.scadalts;
 
 import java.util.concurrent.CompletableFuture;
 
-import org.apache.plc4x.java.api.PlcDriverManager;
-import org.apache.plc4x.java.*;
-import org.apache.plc4x.java.api.*;
-import org.apache.plc4x.java.api.messages.*;
-import org.apache.plc4x.java.api.exceptions.*;
-
 public class Main {
 	public static void main(String[] args) throws Exception {
 		String url = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
