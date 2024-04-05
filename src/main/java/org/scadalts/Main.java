63fda50746b57629f9ba945e41590424225f2ebe
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-05 13:17:32 -0300

    usando threads

--- src/main/java/org/scadalts/Main.java
+++ src/main/java/org/scadalts/Main.java
@@ -16,23 +16,28 @@ import org.slf4j.Marker;
 
 public class Main {
 	public static void main(String[] args) throws PlcConnectionException {
-		String url = "opcua://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
-		System.out.println("Hello");
-		PlcDriver driver = PlcDriverManager.getDefault().getDriver("opcua");
-        /*
-        PlcConnection plcConnection = driver.getConnection(url);
+	}
+	private static Runnable connect = new Runnable() {
+		public void run() {
+			String url = "opcua://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
+			System.out.println("Hello");
+			PlcDriverManager driver = PlcDriverManager.getDefault();
+			//.getDriver("opcua");
+        	/*
+        	PlcConnection plcConnection = driver.getConnection(url);
 
 
-        if(plcConnection.isConnected()) {
-			System.out.println("CONNECTED");
+        	if(plcConnection.isConnected()) {
+				System.out.println("CONNECTED");
+			}
+
+			PlcBrowseRequest plcBrowseRequest = plcConnection.browseRequestBuilder().build();
+			CompletableFuture<? extends PlcBrowseResponse> plcResponse = plcBrowseRequest.execute();
+
+			plcResponse.thenAccept(a -> System.out.println("Res: " + a));
+
+        	 */
 		}
-
-		PlcBrowseRequest plcBrowseRequest = plcConnection.browseRequestBuilder().build();
-		CompletableFuture<? extends PlcBrowseResponse> plcResponse = plcBrowseRequest.execute();
-
-		plcResponse.thenAccept(a -> System.out.println("Res: " + a));
-
-         */
 	}
 }
 
