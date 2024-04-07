59dcffd0ec5a48f95cbfca3037c20ea51cdbf720
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-07 16:11:29 -0300

    getting connection directly from driver

--- src/main/java/org/scadalts/Main.java
+++ src/main/java/org/scadalts/Main.java
@@ -26,7 +26,7 @@ public class Main {
 			System.out.println("Starting Thread");
 			try {
 				PlcDriver driver = PlcDriverManager.getDefault().getDriver("opcua");
-				PlcConnection plcConnection = driver.getConnectionManager().getConnection(url);
+				PlcConnection plcConnection = driver.getConnection(url);
 				System.out.println("Connected with success!");
 			} catch (Exception ex) {
 				System.out.println("We got an error while connecting to plc");
