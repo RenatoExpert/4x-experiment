3a0dff66587a4771c9b671385dff4991082fc979
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-07 16:27:13 -0300

    fix url protocol format

--- src/main/java/org/scadalts/Main.java
+++ src/main/java/org/scadalts/Main.java
@@ -22,7 +22,7 @@ public class Main {
 	}
 	private static Runnable connect = new Runnable() {
 		public void run() {
-			String url = "opc.tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
+			String url = "opcua:tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
 			System.out.println("Starting Thread");
 			try {
 				PlcDriver driver = PlcDriverManager.getDefault().getDriver("opcua");
