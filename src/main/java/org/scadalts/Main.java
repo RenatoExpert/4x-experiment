2d55d89ce387c4ba41ac6a8290307e8f238d6e75
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-07 18:09:10 -0300

    using discovery=false

--- src/main/java/org/scadalts/Main.java
+++ src/main/java/org/scadalts/Main.java
@@ -23,7 +23,7 @@ public class Main {
 	private static Runnable connect = new Runnable() {
 		public void run() {
 			//String url = "opcua:tcp://ec2-3-93-58-9.compute-1.amazonaws.com:4840/";
-			String url = "opcua:tcp://opcuaserver.com:48010/";
+			String url = "opcua:tcp://opcuaserver.com:48010/?discovery=false";
 			System.out.println("Starting Thread");
 			try {
 				PlcDriver driver = PlcDriverManager.getDefault().getDriver("opcua");
