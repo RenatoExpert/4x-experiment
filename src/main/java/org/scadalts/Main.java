f146ae6e0c7ad426f37e5392dbe34ea5c8033c16
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-08 17:41:16 -0300

    make new class Socket

--- src/main/java/org/scadalts/Main.java
+++ src/main/java/org/scadalts/Main.java
@@ -1,42 +1,13 @@
 package org.scadalts;
 
-import org.apache.plc4x.java.api.PlcConnection;
-import org.apache.plc4x.java.api.messages.PlcReadRequest;
-import org.apache.plc4x.java.api.messages.PlcResponse;
-import org.apache.plc4x.java.api.PlcDriver;
-import org.apache.plc4x.java.api.PlcDriverManager;
-import org.apache.plc4x.java.DefaultPlcDriverManager;
-import org.apache.plc4x.java.api.exceptions.PlcConnectionException;
-import org.apache.plc4x.java.api.messages.PlcReadRequest;
-import org.apache.plc4x.java.api.messages.PlcReadResponse;
-import org.apache.plc4x.java.api.messages.PlcBrowseRequest;
-import org.apache.plc4x.java.api.messages.PlcBrowseResponse;
-import org.apache.plc4x.java.api.metadata.PlcDriverMetadata;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Marker;
-
+import org.scadalts.Socket;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Starting App");
-		new Thread(connect).start();
+		new Thread(Socket.connect).start();
 	}
-	private Runnable connect = new Runnable() {
-		public void run() {
-			String url = "opcua:tcp://opcuaserver.com:48010?discovery=false";
-			System.out.println("Starting Thread");
-			try {
-				PlcConnection connection = PlcDriverManager.getDefault().getConnectionManager().getConnection(url);
-				System.out.println(connection.getMetadata().isReadSupported());
-
-				PlcReadRequest request = connection.readRequestBuilder().build();
-				CompletableFuture<? extends PlcResponse> plcResponse = request.execute();
-				PlcResponse response = plcResponse.get(5000, TimeUnit.MILLISECONDS);
+}
 				
 
 				/*
@@ -74,11 +45,6 @@ public class Main {
 					System.out.println("Not connected for unknown reason");
 				}
 				*/
-			} catch (Exception ex) {
-				System.out.println("We got an error while connecting to plc");
-				System.out.println(ex.getMessage());
-				System.out.println(ex);
-			}
         	/*
 			PlcBrowseRequest plcBrowseRequest = plcConnection.browseRequestBuilder().build();
 			CompletableFuture<? extends PlcBrowseResponse> plcResponse = plcBrowseRequest.execute();
@@ -89,7 +55,4 @@ public class Main {
 			CompletableFuture<? extends PlcBrowseResponse> plcResponse = plcBrowseRequest.execute();
 			plcResponse.thenAccept(a -> System.out.println("Res: " + a));
         	 */
-		}
-	};
-}
 
