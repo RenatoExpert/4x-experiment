c14a816f0fab28a87065c01031b230c479062753
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2024-04-17 11:15:04 -0300

    parsing @links as argument

--- get_source.pl
+++ get_source.pl
@@ -17,7 +17,9 @@ sub get_links {
 sub write_list {
 	my $output_file = 'links.txt';
 	open(OF, '>>', $output_file);
-	print OF $_[0] or die($!);
+	foreach $link (@_) {
+		print OF $link or die($!);
+	}
 	close(OF);
 }
 
